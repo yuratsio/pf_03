b44dddc133e7780118b4ef64255a4906e22c1219
yuratsio <93855185+yuratsio@users.noreply.github.com>
2021-11-21 20:14:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,19 +7,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // ==============================
   const body = document.querySelector('body');
   const header = document.querySelector('#header');
+  const headerHeight = header.clientHeight;
 
-  // ==============================
-  // scroll event
-  // ==============================
-  // ページトップからスクロール時にヘッダーを変更する
-  window.addEventListener('scroll', () =>{
-    const topY = window.pageYOffset;
-    if(topY === 0) {
-      header.classList.add('header--top');
-    } else {
-      header.classList.remove('header--top');
-    }
-  });
   // ==============================
   // fade in
   // ==============================
@@ -145,37 +134,6 @@ window.addEventListener('DOMContentLoaded', () => {
       featureImg[index].classList.remove('feature__item-img--hover');
     });
   });
-  // ==============================
-  // news tab
-  // ==============================
-  const newsTabTitles = document.querySelectorAll('.news__tab-title');
-  const newsContents = document.querySelectorAll('.news__list');
-  const newsTab = document.querySelector('.news__tab');
-
-  newsTabTitles.forEach(newsTabTitle => {
-    newsTabTitle.addEventListener('click', () => {
-      // タブタイトルからアクティブクラスを外す
-      newsTabTitles.forEach(newsTabTitle => {
-        newsTabTitle.classList.remove('news__tab-title--active');
-      });
-      // クリックされたタイトルにアクティブクラスをつける
-      newsTabTitle.classList.add('news__tab-title--active');
-      
-      // クリックしたタイトルに応じたタブの表示
-      newsContents.forEach(newsContent => {
-        newsContent.classList.remove('news__list--active');
-        const targetTabContent = document.querySelector('#' + newsTabTitle.dataset.id);
-        targetTabContent.classList.add('news__list--active'); 
-        
-        // タブの高さを取得
-        const newsTab = document.querySelector('.news__tab');
-        newsTab.style.height = targetTabContent.clientHeight + 'px';
-      });
-    });
-  });
-
-  // タブの高さの初期表示
-  newsTab.style.height = newsContents[0].clientHeight + 'px';
 
   // ==============================
   // window resize
@@ -184,12 +142,6 @@ window.addEventListener('DOMContentLoaded', () => {
     // スマホメニューのヘッダー分の余白再取得
     const headerHeight = header.clientHeight;
     hamburgerSlide.style.paddingTop = headerHeight + 40 + 'px';
-
-
-    // アクティブになっているタブの高さを再取得
-    const newsTab = document.querySelector('.news__tab');
-    const newsTabActive = document.querySelector('.news__list--active');
-    newsTab.style.height = newsTabActive.clientHeight + 'px';
   });
 
   // タブレットサイズに広がった時にリロードする（スマホメニュー開時のオーバーフロー指定を解除する）
